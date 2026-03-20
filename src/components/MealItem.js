670df9dd7a890f688cbb7f0286d6aedf6ca480ce
oskarpadu <oskar.padu@voco.ee>
2026-03-20 12:56:54 +0200

    ÜL 4, kuva saadud andmed MealItem komponendi abil

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,15 +1,15 @@
 const MealItem = (props) => {
     return (
-        <li>
-            <article>
-                <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
+        <li className="meal-item">
+            <article className="meal-item-article">
+                <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name} className="meal-item-img"/>
                 <div>
-                    <h3>{props.meal.name}</h3>
-                    <p>props.meal.price</p>
-                    <p>{props.meal.description}</p>
+                    <h3 className="meal-item h3">{props.meal.name}</h3>
+                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p>
-                    <button >Add to Cart</button>
+                    <button>Add to Cart</button>
                 </p>
             </article>
         </li>
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
 
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+
     useEffect(() => {
         fetch("http://localhost:3001/meals")
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => {
+                console.log(data);
+                setMeals(data);
+            });
     }, []);
 
     return (
         <ul id="meals">
+            {meals.map(meal => (
+                <MealItem key={meal.id} meal={meal} />
+            ))}
         </ul>
     );
 };
